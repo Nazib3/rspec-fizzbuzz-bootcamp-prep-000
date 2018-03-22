5b4acd164caf7ecc24a3219c6df0ff994fb0801a
Nazibul Haque <elegant.nazib@gmail.com>
2018-03-22 07:20:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,16 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-  return  "Fizz"
+  fizz = int % 3 == 0
+  buzz = int % 5 == 0
+  fizzbuzz = int % 5 == 0 && int % 3 == 0
+  if fizzbuzz
+    return "FizzBuzz"
+  elsif fizz
+    return "Fizz"
+  elsif buzz
+    return "Buzz"
+  else 
+    return nil
   end
-elsif int % 5 == 0
-  return "Buzz"
-end
-if int % 3==0 && int % 5 == 0
-return "FizzBuzz"
-end
-else
-  return nil
-end
+     
+    
 end
